653185d8a3f4ff939848a7a63858d9f37f10e323
simplexxagency <ant@simplexx.agency>
2019-12-03 11:12:44 +0200

    add new blocks

--- src/js/script.js
+++ src/js/script.js
@@ -20,18 +20,18 @@ $(document).ready(function () {
             if ( percentage < 0.3) {
                 $heroDesc.css('opacity', '1').css('transition', 'all ease 2s');
                 $('.hero__bottom').css('opacity', '1').css('transition', 'all ease 2s');
+                $('.hero__img').css('transform', 'scale(1)').css('transition', 'all ease 2s');
             }
             if ( percentage > 0.3) {
                 $('.hero__bottom').css('opacity', 1 - percentage).css('transition', 'all ease 2s');
                 // $heroDesc.css('opacity', '1');
                 $heroDesc.css('opacity', 1 - percentage).css('transition', 'all ease 2s');
+                $('.hero__img').css('transform', 'scale(1.2)').css('transition', 'all ease 2s');
             } 
             if ( percentage < 0.5 ) {
-                $('.hero__img').css('transform', 'scale(1)').css('transition', 'all ease 2s');
                 $('.hero__bottom-mouse').css('z-index', '1');
             }
             if ( percentage > 0.5 ) {
-                $('.hero__img').css('transform', 'scale(1.2)').css('transition', 'all ease 2s'); 
                 $heroDesc.css('opacity', '0').css('transition', 'all ease 1s');
                 $('.hero__bottom').css('opacity', '0').css('transition', 'all ease 1s');
                 $('.hero__bottom-mouse').css('z-index', '0');
@@ -53,5 +53,22 @@ $(document).ready(function () {
             $heroFade.css('opacity', '0');
             $heroDesc.css('opacity', '1');
         }
+    });
+
+    // Menu in Header
+    let $btn = $('.header__menu-line');
+    let $navMenu = $('.header__menu-mobile');
+
+    $('.header__menu-btn, .header__item-mobile').on('click', function () {
+        if (!($btn.hasClass('active'))) {
+            $btn.addClass('active');
+            $navMenu.addClass('active');
+            $('body').toggleClass('active');
+        } else {
+            $btn.removeClass('active');
+            $navMenu.removeClass('active');
+            $('body').toggleClass('active');
+        }
+
     });
 });
